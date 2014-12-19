529edf59b81f2e25435964e2cba249d0f7640e9b
Michał Moskała <moskala.michal@gmail.com>
2014-12-19 20:27:45 +0100

    TyphoonFramework + CocoaPods.

---
+++ MyBudget-Bridging-Header.h
@@ -0,0 +1,9 @@
+//
+//  MyBudget-Bridging-Header.h
+//  MyBudget
+//
+//  Created by Michal Moskala on 19/12/14.
+//  Copyright (c) 2014 Michal Moskala. All rights reserved.
+//
+
+#import "Typhoon.h"
