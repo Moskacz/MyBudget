82c46017a781adcbdedd6cd2e30db5f477a8ce69
Michał Moskała <moskala.michal@gmail.com>
2015-01-03 23:57:20 +0100

    Adding spending with no value blocked (button disabled).

--- MyBudget/Controllers/AddSpendingViewController.swift
+++ MyBudget/Controllers/AddSpendingViewController.swift
@@ -10,13 +10,22 @@ import UIKit
 
 class AddSpendingViewController: UIViewController {
 	
+	@IBOutlet weak var addSpendingButton: UIButton!
 	@IBOutlet weak var datePicker: UIDatePicker!
 	@IBOutlet weak var value: UITextField!
 	var dataManager: SpendingsDataManager!
 	
   override func viewDidLoad() {
-    super.viewDidLoad()		
+    super.viewDidLoad()
+		addSpendingButton.enabled = false
+		value.delegate = self
 	}
+	
+	private func dismissController() {
+		dismissViewControllerAnimated(true, completion: nil)
+	}
+	
+	// MARK: Button Events Handling
 	
 	@IBAction func addButtonTapped(sender: UIButton) {
 		if value.text.utf16Count != 0 {
@@ -24,7 +33,25 @@ class AddSpendingViewController: UIViewController {
 			let spendingDTO = SpendingDTO(date: self.datePicker.date, value: NSNumber(float: value))
 			self.dataManager.saveSpending(spendingDTO)
 		}
-		
-		dismissViewControllerAnimated(true, completion: nil)
+		dismissController()
+	}
+	
+	@IBAction func dismissButtonTapped(sender: UIButton) {
+		dismissController()
+	}
+	
+}
+
+extension AddSpendingViewController: UITextFieldDelegate {
+	
+	// MARK: UITextFieldDelegate
+	
+	func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+		if string == "" {
+			self.addSpendingButton.enabled = textField.text.utf16Count > 1
+		} else {
+			self.addSpendingButton.enabled = (textField.text.utf16Count + string.utf16Count) > 0
+		}
+		return true
 	}
 }
